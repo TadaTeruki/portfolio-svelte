567c4ee848cd0a6b5512d0e991dcf195ad094126
Teruki TADA <me@peruki.dev>
2024-05-05 19:20:00 +0900

    snapshot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,16 @@
 
 <div class="title-space"></div>
 
+<h2>About Me</h2>
+
+<div class="content">
+    I am a hobbyist developer from western Japan, 
+    and moved to Hakodate for university and my interest in moving around.
+    I love maps and geography, and most of my work is related to them, 
+    such as procedural generation of geographic features. 
+    I am also interested in WebGIS and web development as tools for expression. 
+</div>
+
 <h2>Educations</h2>
 
 <div class="content">
