d42569c191e53459384823c9a99783924d53cf78
Teruki TADA <me@peruki.dev>
2024-05-05 13:40:36 +0900

    fundamentals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>Teruki TADA's Webpage</header>
+
+<main>
+    <h1>Teruki TADA 多田 瑛貴</h1>
+    <p>A Hobbiest Developer in Hakodate, Japan</p>
+</main>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New&display=swap');
+
+    :global(body) {
+        font-family: 'Zen Kaku Gothic New', sans-serif;
+        margin: 0;
+    }
+
+    header {
+        background-color: #333;
+        color: white;
+        padding: 1em;
+    }
+
+    main {
+        padding: 5em;
+        margin: 0 auto;
+        max-width: 80em;
+    }
+</style>
